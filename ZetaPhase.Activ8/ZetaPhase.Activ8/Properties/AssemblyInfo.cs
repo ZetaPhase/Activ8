1491404e877e18e2cc0310063cf0250f6e0053af
0xFireball <0xFireball@outlook.com>
2016-04-27 09:09:32 -0700

    update assemblyinfo

--- ZetaPhase.Activ8/ZetaPhase.Activ8/Properties/AssemblyInfo.cs
+++ ZetaPhase.Activ8/ZetaPhase.Activ8/Properties/AssemblyInfo.cs
@@ -1,31 +1,28 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZetaPhase.Activ8")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZetaPhase.Activ8")]
-[assembly: AssemblyCopyright("Copyright 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("ZetaPhase.Activ8")]
+[assembly: AssemblyDescription ("The Activ8 Library for SharpJS")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("ZetaPhase Tenchnologies, Inc.")]
+[assembly: AssemblyProduct ("ZetaPhase.Activ8")]
+[assembly: AssemblyCopyright ("Copyright 2016 ZetaPhase Technologies, Inc.")]
+[assembly: AssemblyTrademark ("Activ8")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: Guid ("5c917a5f-71ad-4d68-93bd-86e479fd9072")]
